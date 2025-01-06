f9be43fa6de376f3976b7ed667e8a7c053a05cd9
YvesLubalika <yveslubalika02@gmail.com>
2025-01-07 00:23:38 +0200

    Update Projet_groupe1_Apps.py

--- Projet_groupe1_Apps.py
+++ Projet_groupe1_Apps.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error,r2_score
 import joblib
